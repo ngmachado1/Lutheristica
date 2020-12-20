14d9ae2601529428e7852591411132da7cfc1760
ngmachado1 <54647104+ngmachado1@users.noreply.github.com>
2020-12-20 14:24:17 -0300

    comenzando con firebase

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+ //MENU
 function menu(){
     const menu = document.querySelector('.menu');
     const line1 = document.querySelector('.line1');
@@ -6,7 +7,6 @@ function menu(){
     const lista = document.querySelectorAll('.lista')
     const ulCenter = document.querySelector('#center')
 
-    console.log(ulCenter)
     
     menu.addEventListener('click', ()=>{
         if(line1.className=="line1",
@@ -34,16 +34,49 @@ function menu(){
         }
     })
 }
+window.addEventListener('DOMContentLoaded', ()=>{
+    menu();
+
+    new Splide( '.splide', {
+        type   : 'loop',
+        rewind     : true,
+        heightRatio: 0.5,
+        pagination : false,
+        focus    : 'center'
+    } ).mount();
+
+    getItems();
+})
 
 
-menu();
 
 
-new Splide( '.splide', {
-    type   : 'loop',
-    rewind     : true,
-    heightRatio: 0.5,
-    pagination : false,
-    focus    : 'center'
-} ).mount();
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyB6wT0bHOhPS7lBhMEyXGWZrfrKeVolNW0",
+    authDomain: "redlutheristica.firebaseapp.com",
+    projectId: "redlutheristica",
+    storageBucket: "redlutheristica.appspot.com",
+    messagingSenderId: "595854357739",
+    appId: "1:595854357739:web:8aa696869f4007bceb7aae",
+    measurementId: "G-DZPF6H3BEX"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+  const db=firebase.database();
+  const dbref=db.ref().child('articulos');
 
+  const getItems = ()=>{
+    dbref.on('value',function(snapshot){
+  
+        snapshot.forEach(function(childSnapshot) {
+    
+            console.log(childSnapshot)
+    
+            console.log(childSnapshot.val())
+        })
+    
+    })
+  }  
