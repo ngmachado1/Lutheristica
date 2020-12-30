061e4c5e870518d5a1a678458384045867319422
ngmachado1 <nataliagisellemachado@gmail.com>
2020-12-30 16:44:05 -0300

    creando selects

--- js/admin.js
+++ js/admin.js
@@ -1,65 +1,79 @@
-const paises_component = (pais) =>{
-    return `<option value="${pais}">${pais}</option>`
+const select_component = (nombre) =>{
+    return `<option value="${nombre}">${nombre}</option>`
 }
 
-const paises_render = (containerId, array) =>{
+const select_render = (containerId, array) =>{
     let container = document.getElementById(containerId);
     container.innerHTML = "";
-    let html = '<option value="">Elegir Pais</option>';
-    array.forEach(pais => {
-        html = html + paises_component(pais)
+    let html = '<option value="">Elegir lugar</option>';
+    array.forEach(nombre => {
+        html = html + select_component(nombre)
     });
     container.innerHTML = html;
 }
+
+
+//crando paises
+function ajaxPais(){
+    let url = `paises.Json`;
+    var paises = [];
+    $.ajax({
+        method: "GET",
+        url: url
+    }).done(function(data){
+
+        data.forEach(Element => {
+         
+        paises.push(Element.name)
+          
+        select_render('paises', paises);
+          
+        });
+    }).fail(function(error){
+        error = alert('no se establecio la conexion');
+    })
+}
+
+//creo provincias
+
+function ajaxProvincia(){
+    let selectpais = document.getElementById('paises')
+    selectpais.addEventListener('change', () =>{
+        let pais = selectpais.value
+        
+        let url = `paises.Json`;
+        var provincias = [];
+        $.ajax({
+            method: "GET",
+            url: url
+        }).done(function(data){
+            data.forEach(Element => {
+            if(Element.name == pais){
+                var elef = Element.provincias
+                for (let index = 0; index < elef.length; index++) {
+                    const element = elef[index].name;
+                   provincias.push(element)
+                }
+                select_render('provincias', provincias)
+            }
+            });
+        }).fail(function(error){
+            error = alert('no se establecio la conexion');
+        })
+    
+    })
+
+}
+
+
 window.onload = function () {
 
-    function ajaxPais(){
-        let url = `https://restcountries.eu/rest/v2/all`;
-        var paises = [];
-        $.ajax({
-            method: "GET",
-            url: url
-        }).done(function(data){
-    
-            data.forEach(Element => {
-              if (Element.subregion == "South America" || Element.subregion == "Central America") {
-                paises.push(Element.name)
-              }
-              paises_render('paises', paises);
-              
-            });
-        }).fail(function(error){
-            error = alert('no se establecio la conexion');
-        })
-    }
+
+
     ajaxPais();
 
-    function ajaxProvincia(){
-        let url = `https://restcountries.eu/rest/v2/all`;
-        var paises = [];
-        $.ajax({
-            method: "GET",
-            url: url
-        }).done(function(data){
-    
-            data.forEach(Element => {
-              if (Element.subregion == "South America" || Element.subregion == "Central America") {
-                paises.push(Element.name)
-              }
-              paises_render('paises', paises);
-              
-            });
-        }).fail(function(error){
-            error = alert('no se establecio la conexion');
-        })
 
-        var select = document.getElementById('paises');
-        select.addEventListener('change',
-          function(){
-            var selectedOption = this.options[select.selectedIndex];
-            console.log(selectedOption.value + ': ' + selectedOption.text);
-          });
-    }
+    ajaxProvincia();
     
 
 
